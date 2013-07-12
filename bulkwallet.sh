86f139b71166f3c38a01451fe48ed92b08068df9
U-CATHOLICCHARITI\salfter <salfter@SALFTER_W7.catholiccharities.com>
2013-07-12 11:13:40 -0700

    some bulk wallet optimization

--- bulkwallet.sh
+++ bulkwallet.sh
@@ -9,4 +9,4 @@
 # 23  Primecoin
 # 48  Litecoin
 
-./vanitygen -kF compressed -X $1 $2 2>/dev/null | egrep "Address|Privkey" | awk '{printf("%s ", $2); getline; printf("%s\n",$2)}' | head -n $3
+./vanitygen -kF compressed -X $1 $2 2>/dev/null | head -n `expr $3 \* 3` | egrep "Address|Privkey" | awk '{printf("%s ", $2); getline; printf("%s\n",$2)}'
